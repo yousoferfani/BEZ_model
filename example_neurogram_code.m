42c5e65dee54d9aca2fb0d6eb7de5a33c799b00a
yousoferfani <erfani.yousof@gmail.com>
2018-01-12 22:06:50 -0500

    BEZ2018

---
+++ example_neurogram_code.m
@@ -0,0 +1,51 @@
+% Check to see if running under Matlab or Octave
+if exist ('OCTAVE_VERSION', 'builtin') ~= 0
+  pkg load signal;
+  if exist('rms')<1
+    rms = @(x) sqrt(mean(x.^2));
+  end
+end
+
+% Set audiogram
+ag_fs = [125 250 500 1e3 2e3 4e3 8e3];
+ag_dbloss = [0 0 0 0 0 0 0]; % Normal hearing
+
+% Set audiogram
+% ag_fs = [125 250 500 1e3 2e3 4e3 8e3];
+% ag_dbloss = [0 0 0 20 40 60 80]; % Example high-freq hearing loss
+
+species = 2; % Human cochlear tuning (Shera et al., 2002)
+
+[stim, Fs_stim] = audioread('defineit.wav');
+
+stimdb = 65; % speech level in dB SPL
+
+stim = stim/rms(stim)*20e-6*10^(stimdb/20);
+
+[neurogram_ft,neurogram_mr,t_ft,t_mr,CFs] = generate_neurogram_BEZ2018(stim,Fs_stim,species,ag_fs,ag_dbloss);
+
+ng=figure;
+set(ng,'renderer','painters');
+winlen = 256; % Window length for the spectrogram analyses
+sp1 = subplot(3,1,1);
+[s,f,t] = specgram([stim; eps*ones(round(t_mr(end)*Fs_stim)-length(stim),1)],winlen,Fs_stim,winlen,0.25*winlen);
+imagesc(t,f/1e3,20*log10(abs(s)/sum(hanning(winlen))*sqrt(2)/20e-6));
+axis xy; axis tight;
+hcb = colorbar;
+set(get(hcb,'ylabel'),'string','SPL')
+caxis([stimdb-80 stimdb])
+ylim([0 min([max(CFs/1e3) Fs_stim/2e3])])
+xlabel('Time');
+ylabel('Frequency (kHz)');
+title('Spectrogram')
+xl = xlim;
+sp2=subplot(3,1,2);
+plot_neurogram(t_mr,CFs,neurogram_mr,sp2);
+caxis([0 80])
+title('Mean-rate Neurogram')
+xlim(xl)
+sp2=subplot(3,1,3);
+plot_neurogram(t_ft,CFs,neurogram_ft,sp2);
+caxis([0 20])
+title('Fine-timing Neurogram')
+xlim(xl)
